4b474245b0192e68871f59217a2eb50e0ebbc90b
Dmitry Karpenko <dmitry@onpositive.com>
2018-06-08 20:32:01 +0700

    Extend roads generation to support different road types
    Change-Id: If591868e8f8ad81353f62facb606111ff738e724

--- osm2xp/src/com/osm2xp/translators/xplane/XPRoadTranslator.java
+++ osm2xp/src/com/osm2xp/translators/xplane/XPRoadTranslator.java
@@ -11,6 +11,8 @@ import com.osm2xp.writers.IWriter;
 
 public class XPRoadTranslator extends XPPathTranslator {
 	
+	private static final String[] WIDE_ROAD_TYPES = {"motorway", "trunk", "primary", "secondary"}; 
+	private static final String HIGHWAY_TAG = "highway";
 	private String[] allowedHighwayTypes = GuiOptionsHelper.getAllowedHighwayTypes();
 	private String[] allowedHighwaySurfaceTypes = GuiOptionsHelper.getAllowedHighwaySurfaceTypes();
 	public XPRoadTranslator(IWriter writer) {
@@ -22,7 +24,7 @@ public class XPRoadTranslator extends XPPathTranslator {
 		if (!XplaneOptionsHelper.getOptions().isGenerateRoads()) {
 			return false;
 		}
-		if (ArrayUtils.contains(allowedHighwayTypes, poly.getTagValue("highway"))) {
+		if (ArrayUtils.contains(allowedHighwayTypes, poly.getTagValue(HIGHWAY_TAG))) {
 			String surface = poly.getTagValue("surface"); //Generate if surface type is either missing or among allowe values
 			if (StringUtils.stripToEmpty(surface).trim().isEmpty() || ArrayUtils.contains(allowedHighwaySurfaceTypes, surface)) {
 				addSegmentsFrom(poly);
@@ -32,9 +34,38 @@ public class XPRoadTranslator extends XPPathTranslator {
 		return false;
 	}
 
+	/**
+	 * Return X-Plane type constant for given road. Written in "hard" way to make some match between OSM tags and X-Plane type constants
+	 * TODO Should me made configurable in future
+	 * From https://forums.x-plane.org/index.php?/files/file/19074-roads-tutorial/ :
+	 * 4 lanes - type 10
+	 * 2 lanes - type 40
+	 * 1 lane - type 50
+	 */
 	protected int getPathType(OsmPolygon poly) {
-		// TODO Implement actual logics for this, using const value for now
-		return 50;
+		String val = poly.getTagValue("lanes"); //Try to get lane count directly first
+		if (val != null) {
+			try {
+				int value = Integer.parseInt(val.trim());
+				if (value >= 4) {
+					return 10;
+				} else if (value >=2) {
+					return 40;
+				} else {
+					return 50;
+				}
+			}catch (NumberFormatException e) {
+				// ignore
+			}
+		}
+//		String type = poly.getTagValue(HIGHWAY_TAG).toLowerCase(); //If no lane count - guess from the highway type
+//		if (ArrayUtils.indexOf(WIDE_ROAD_TYPES, type) >= 0) {
+//			return 40;
+//		}
+		if ("yes".equalsIgnoreCase(poly.getTagValue("oneWay")) && ArrayUtils.indexOf(WIDE_ROAD_TYPES, poly.getTagValue(HIGHWAY_TAG)) == -1) {
+			return 50; //One-way road is treated as one-lane, if lane count is unspecified, and we doesn't have "wide" road	
+		}
+		return 40; //Use 2 lanes road by default
 	}
 	
 	@Override
